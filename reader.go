e9f89e751ffd7567c469bac3ddde067b69483a2e
tom caruso <carusot42@gmail.com>
2018-01-22 23:33:27 -0500

    better wording for docs

--- reader.go
+++ reader.go
@@ -1,5 +1,4 @@
-// Package wkt provides an easy-to-use wrapper around a WKT file, with options for parsing common formats.
-
+// Package wktfile provides an easy-to-use wrapper around a WKT file, with options for parsing common formats.
 package wktfile
 
 import (
@@ -23,40 +22,38 @@ type WKTFile struct {
 }
 
 
-// Read reads a standard text-based WKTFile file with certain common options available.
+// Read reads and parses a standard text-based WKT file, allowing the caller to specify some common options for parsing.
 // It returns a WKTFile representation of the input file.
 func Read(filepath string, options ...func(*WKTFile) error) (*WKTFile, error) {
-	wktFile := &WKTFile{}
+	wkt := &WKTFile{}
 
 	for _, f := range defaults { // Set default WKTFile options
-		err := f(wktFile)
+		err := f(wkt)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	for _, f := range options { // User options override defaults
-		err := f(wktFile)
+		err := f(wkt)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	fileContents, err := readfile(filepath, wktFile.delimiter)
+	fileContents, err := readfile(filepath, wkt.delimiter)
 	if err != nil {
 		return nil, err
 	}
 
-
-
-	if wktFile.hasheader {
-		wktFile.Header = fileContents[0]
-		wktFile.Rows = fileContents[1:]
+	if wkt.hasheader {
+		wkt.Header = fileContents[0]
+		wkt.Rows = fileContents[1:]
 	} else {
-		wktFile.Rows = fileContents
+		wkt.Rows = fileContents
 	}
 
-	return wktFile, nil
+	return wkt, nil
 }
 
 // readfile is a helper function to get all rows in a file.
@@ -68,7 +65,6 @@ func readfile(filepath string, delimiter rune) ([][]string, error) {
 	if err != nil {
 		return nil, err
 	}
-
 
 	reader := csv.NewReader(file)
 	reader.Comma = delimiter
@@ -82,40 +78,40 @@ func readfile(filepath string, delimiter rune) ([][]string, error) {
 
 }
 
-// PipeDelimiter is an option for wkt.Read() which specifies that the WKT file is delimited by '|' values.
-func PipeDelimiter(_wkt *WKTFile) error {
-	_wkt.delimiter = '|'
+// PipeDelimiter is a functional parameter to Read() which specifies that the WKT file is delimited by '|' values.
+func PipeDelimiter(wkt *WKTFile) error {
+	wkt.delimiter = '|'
 	return nil
 }
 
-// CommaDelimiter is an option for wkt.Read() which specifies that the WKT file is delimited by ',' values.
-func CommaDelimiter(_wkt *WKTFile) error {
-	_wkt.delimiter = ','
+// CommaDelimiter is a functional parameter to Read() which specifies that the WKT file is delimited by ',' values.
+func CommaDelimiter(wkt *WKTFile) error {
+	wkt.delimiter = ','
 	return nil
 }
 
-// TabDelimiter is an option for wkt.Read() which specifies that the WKT file is delimited by '\t' values.
-func TabDelimiter(_wkt *WKTFile) error {
-	_wkt.delimiter = '\t'
+// TabDelimiter is a functional parameter to Read() which specifies that the WKT file is delimited by '\t' values.
+func TabDelimiter(wkt *WKTFile) error {
+	wkt.delimiter = '\t'
 	return nil
 }
 
-// CustomDelimiter is an option for wkt.Read() which allows the user to specify a custom delimiter for a file.
-func CustomDelimiter(delimiter rune) func(_wkt *WKTFile) error {
-	return func(_wkt *WKTFile) error {
-		_wkt.delimiter = delimiter
+// CustomDelimiter isa functional parameter to Read() which allows the user to specify a custom delimiter for a file.
+func CustomDelimiter(delimiter rune) func(wkt *WKTFile) error {
+	return func(wkt *WKTFile) error {
+		wkt.delimiter = delimiter
 		return nil
 	}
 }
 
-// FileHasHeader is an option for wkt.Read() which specifies that the file has a header.
-func FileHasHeader(_wkt *WKTFile) error {
-	_wkt.hasheader = true
+// FileHasHeader is a functional parameter to Read() which specifies that the file has a header.
+func FileHasHeader(wkt *WKTFile) error {
+	wkt.hasheader = true
 	return nil
 }
 
-// FileNotHasHeader is an option for wkt.Read() which specifies that the file does not have a header.
-func FileNotHasHeader(_wkt *WKTFile) error {
-	_wkt.hasheader = false
+// FileNoHeader is a functional parameter to Read() which specifies that the file does not have a header.
+func FileNoHeader(wkt *WKTFile) error {
+	wkt.hasheader = false
 	return nil
 }
--- reader_test.go
+++ reader_test.go
@@ -5,22 +5,100 @@ import (
 	"testing"
 )
 
-const testDataDir = "./testdata/"
+const (
+	testDataDir = "./testdata/"
+)
+
+var (
+	testHeader = []string{"point_id", "wkt"}
+	testContent = [][]string{
+		{"1", "POINT(-72.549738631281 43.724067830418)",},
+		{"2", "POINT(-73.3208999067471 44.9213993671176)",},
+		{"3", "POINT(-72.3267250008435 44.3847434287252)",},
+	}
+)
+
+
+func headersMatch(t *testing.T, wkt *WKTFile) {
+	for i, col := range wkt.Header {
+		if col != testHeader[i] {
+			t.Errorf("Headers do not match")
+		}
+	}
+}
+
+func contentsMatch(t *testing.T, wkt *WKTFile) {
+	for rIdx, row := range wkt.Rows {
+		for cIdx, value := range row {
+			if value != testContent[rIdx][cIdx] {
+				t.Errorf("File contents do not match")
+			}
+		}
+	}
+}
 
 
 
-func TestReadDefaultOptions(t *testing.T) {
+func TestReadDefault(t *testing.T) {
 	testFile := testDataDir + "test_points.wkt"
-	testHeader := []string{"point_id", "wkt"}
-
 
 	wkt, err := Read(testFile)
 	if err != nil {
 		t.Errorf("Could not read file")
 	}
 
-	for i := range wkt.Header {
-		
+	headersMatch(t, wkt)
+	contentsMatch(t, wkt)
+
+}
+
+func TestReadComma(t *testing.T) {
+	testFile := testDataDir + "test_points_comma.wkt"
+
+	wkt, err := Read(testFile, CommaDelimiter)
+	if err != nil {
+		t.Errorf("Could not read file")
 	}
+
+	headersMatch(t, wkt)
+	contentsMatch(t, wkt)
+
+}
+
+func TestReadTab(t *testing.T) {
+	testFile := testDataDir + "test_points_tab.wkt"
+
+	wkt, err := Read(testFile, TabDelimiter)
+	if err != nil {
+		t.Errorf("Could not read file")
+	}
+
+	headersMatch(t, wkt)
+	contentsMatch(t, wkt)
+
+}
+
+func TestReadCustom(t *testing.T) {
+	testFile := testDataDir + "test_points_custom.wkt"
+
+	wkt, err := Read(testFile, CustomDelimiter(';'))
+	if err != nil {
+		t.Errorf("Could not read file")
+	}
+
+	headersMatch(t, wkt)
+	contentsMatch(t, wkt)
+
+}
+
+func TestReadNoHeader(t *testing.T) {
+	testFile := testDataDir + "test_points_noheader.wkt"
+
+	wkt, err := Read(testFile, FileNoHeader)
+	if err != nil {
+		t.Errorf("Could not read file")
+	}
+
+	contentsMatch(t, wkt)
 
 }
